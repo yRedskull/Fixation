065e93db250472eb3e6fd9f5a53e58f218545374
T. Felipe <eusouored@hotmail.com>
2023-04-06 23:57:33 -0300

    Att

--- run.py
+++ run.py
@@ -3,9 +3,11 @@ from auto_update import Auto_Update
 from threading import Thread
 from var import *
 
-if __name__ == "__main__":
-
+def Run():
     if on_config_json["Version"] != config["Version"]:
         Thread(target=Auto_Update()).start()
     else:
         Thread(target=Start()).start()
+
+if __name__ == '__main__':
+    Run()
