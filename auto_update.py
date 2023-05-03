bdd3fbf9827c2ab7e3693edbdb9caa7d039e347f
T. Felipe <eusouored@hotmail.com>
2023-05-02 22:03:25 -0300

    Update

--- auto_update.py
+++ auto_update.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter.ttk import Progressbar
+from interface.app import Start
 from var import on_config_json, config
+from engine.update import Update_progress
 from threading import Thread
-from engine.update import Update_file, Restart
 
 
 class Auto_Update:
     def __init__(self, bag=config["Background-Color-APP"], fog=config["Foreground-Color"], fts=config["Font"]):
         try: 
             self.upt = Tk()
+
             self.upt.title('Auto-Update')
             self.upt.iconphoto(False, PhotoImage(file="image/logo.png", master=self.upt))
             self.upt.configure(background=bag)
@@ -16,6 +18,7 @@ class Auto_Update:
             self.auto_update = dict(on_config_json["Auto-Update"])
             self.url_base = self.auto_update["url"]
             self.files = self.auto_update["folders-files"]
+            self.restart_val = False
 
             self.width_screen = self.upt.winfo_screenwidth()
             self.height_screen = self.upt.winfo_screenheight()
@@ -34,21 +37,25 @@ class Auto_Update:
             self.progressbar.pack(ipadx=200, ipady=6, padx=3, pady=3)
 
             def configbgcolor(event):
-                self.verificar.config(bg='#333')
+                self.verify.config(bg='#333')
                 return event
 
             def configbgbag(event):
-                self.verificar.config(bg=bag)
+                self.verify.config(bg=bag)
                 return event
             
-            self.verificar = Button(self.upt, text="Atualizar",command=lambda: Thread(target=lambda: Update_file(self, bag, fog, fts)).start(),
+            self.verify = Button(self.upt, text="Atualizar",command=lambda: Thread(target=Update_progress(self, bag, fog, fts)).start(),
                                      bg=bag, fg=fog,font=fts, highlightthickness=0, border=1)
-            self.verificar.pack(side="bottom", padx=4, pady=4, ipadx=10, ipady=3)
-            self.verificar.bind("<Enter>", configbgcolor)
-            self.verificar.bind("<Leave>", configbgbag)
+            self.verify.pack(side="bottom", padx=4, pady=4, ipadx=5, ipady=1)
+            self.verify.bind("<Enter>", configbgcolor)
+            self.verify.bind("<Leave>", configbgbag)
             self.upt.update()
 
             self.upt.mainloop()
+
+            if not self.restart_val:
+                Start()
+            
         except Exception:
             pass
 
--- engine/update.py
+++ engine/update.py
@@ -1,57 +1,59 @@
 from urllib import request
 from tkinter import Button
-from threading import Thread
+from interface.app import Start
 import subprocess, os
 
 
-def Update_file(self, bag, fog, fts):
-        self.verificar.destroy()
-        
-        cont = 0
-        for file in self.files:
-            self.Varp.set(cont)
-            try:        
-                url_online_file = os.path.join(self.url_base, file)
-                url_local_file = os.path.join(os.getcwd(), file)
-                if file.count('/') >= 1:
-                    for pos, l in enumerate(file):
-                        if '/' == file[pos]:
-                            pasta = file[:pos]
-                            break
-                    if not os.path.exists(os.path.join(os.getcwd(), pasta)):
-                        os.mkdir(pasta)
-                request.urlretrieve(url_online_file, url_local_file)
-
-            except:
-                cont += 1
-                continue
-            self.upt.update()
+def Update_progress(self, bag, fog, fts):
+    self.verify.destroy()
+    
+    cont = 0
+    for file in self.files:
+        self.Varp.set(cont)
+        try:        
+            Update(self)
+        except:
             cont += 1
-        self.Varp.set(len(self.files))    
+            continue
         self.upt.update()
+        cont += 1
+    self.Varp.set(len(self.files))    
+    self.upt.update()
 
-        def configbgcolor(event):
-                self.restart.config(bg="#333")
-                return event
-
-        def configbgbag(event):
-            self.restart.config(bg=bag)
+    def configbgcolor(event):
+            self.restart.config(bg="#333")
             return event
-        
-        self.restart = Button(self.upt, text="Reiniciar",command=lambda: Restart(self),
-                              bg=bag, fg=fog,font=fts, highlightthickness=0, bd=1)
-        self.restart.pack(side="bottom", padx=2, pady=2, ipadx=10, ipady=3)
-        self.restart.bind("<Enter>", configbgcolor)
-        self.restart.bind("<Leave>", configbgbag)
 
-        self.upt.update()
+    def configbgbag(event):
+        self.restart.config(bg=bag)
+        return event
+    
+    self.restart = Button(self.upt, text="Reiniciar",command=lambda: Restart(self),
+                            bg=bag, fg=fog,font=fts, highlightthickness=0, bd=1)
+    self.restart.pack(side="bottom", padx=2, pady=2, ipadx=10, ipady=3)
+    self.restart.bind("<Enter>", configbgcolor)
+    self.restart.bind("<Leave>", configbgbag)
+
+    self.upt.update()
+
+def Update(self, file):
+    url_online_file = os.path.join(self.url_base, file)
+    url_local_file = os.path.join(os.getcwd(), file)
+    if file.count('/') >= 1:
+        for pos, l in enumerate(file):
+            if '/' == file[pos]:
+                pasta = file[:pos]
+                break
+        if not os.path.exists(os.path.join(os.getcwd(), pasta)):
+            os.mkdir(pasta)
+    request.urlretrieve(url_online_file, url_local_file)
     
 def Restart(self):
-        self.upt.destroy()
-        if os.path.exists('Fixation.exe'):
-            Thread(target=subprocess.call('Fixation.exe', shell=True))
-        else:
-            Thread(target=subprocess.call('run.py', shell=True))
+    self.upt.destroy()
 
-if __name__ == "__main__":
-    Thread(target=subprocess.call('auto_update.pyw', shell=True))
+    if os.path.exists('Fixation.exe'):
+        self.restart_val = True
+        subprocess.call('Fixation.exe', shell=True)
+    else:
+        self.restart_val = True
+        Start()
